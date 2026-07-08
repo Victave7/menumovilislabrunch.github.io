f9b97a8ede896650e5de8dcccd237b030f51e712
Victave7 <107119440+Victave7@users.noreply.github.com>
2026-07-08 11:24:32 +0200

    au

--- club/app.js
+++ club/app.js
@@ -1,3 +1,6 @@
+console.log("ISLA CLUB APP FUNCIONANDO");
+
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { 
@@ -18,81 +21,56 @@ storageBucket: "club-2e4c7.firebasestorage.app",
 
 messagingSenderId: "719625587140",
 
-appId: "1:719625587140:web:f0b0141c8120d580303c91",
-
-measurementId: "G-WZBDR9Y3YZ"
+appId: "1:719625587140:web:f0b0141c8120d580303c91"
 
 };
 
 
-
 const app = initializeApp(firebaseConfig);
-
 
 const auth = getAuth(app);
 
 
-
-const boton = document.getElementById("join");
-
+document.getElementById("join").onclick = async function(){
 
 
-boton.addEventListener("click", async ()=>{
+console.log("BOTON FUNCIONA");
 
 
-const nombre = document.getElementById("nombre").value;
+let email = document.getElementById("email").value;
 
-const email = document.getElementById("email").value;
-
-const password = document.getElementById("password").value;
+let password = document.getElementById("password").value;
 
 
-
-if(!nombre || !email || !password){
-
-document.getElementById("mensaje").innerHTML =
-"Completa todos los campos";
-
-return;
-
-}
+console.log(email,password);
 
 
 
-try{
+try {
 
 
-const usuario = await createUserWithEmailAndPassword(
+await createUserWithEmailAndPassword(
 auth,
 email,
 password
 );
 
 
-
-document.getElementById("mensaje").innerHTML =
-"🌴 Bienvenido al Isla Club " + nombre;
-
-
-
-document.getElementById("nombreTarjeta").innerHTML =
-nombre;
-
-
-
-console.log(usuario);
-
-
-
-}catch(error){
-
-
-document.getElementById("mensaje").innerHTML =
-error.message;
+alert("Usuario creado correctamente 🌴");
 
 
 }
 
 
+catch(error){
 
-});
+
+console.log(error);
+
+alert(error.message);
+
+
+}
+
+
+}
